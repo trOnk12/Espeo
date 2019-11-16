9b9bb6745df3a1693c5cf3d301c7668a1b36b034
trOnk12 <m.pachulski94@gmail.com>
2019-11-16 18:58:41 +0100

    renamed mainactivity

--- app/src/main/java/com/example/espeo/presentation/StudentListActivity.kt
+++ app/src/main/java/com/example/espeo/presentation/StudentListActivity.kt
@@ -4,10 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.espeo.R
 
-class MainActivity : AppCompatActivity() {
+class StudentListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.student_list_activity)
     }
 }
