2e77f68c0fa982763ffd746f0a075b74ffb74379
trOnk12 <m.pachulski94@gmail.com>
2019-11-17 15:11:04 +0100

    added observeOn to set live data value on the proper thread

--- app/src/main/java/com/example/espeo/feature/presentation/StudentListViewModel.kt
+++ app/src/main/java/com/example/espeo/feature/presentation/StudentListViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import com.example.espeo.core.platform.BaseViewModel
 import com.example.espeo.feature.domain.model.Student
 import com.example.espeo.feature.domain.usecase.GetStudents
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
@@ -16,7 +17,9 @@ class StudentListViewModel
 
     fun getStudents() {
         compositeDisposable.add(
-            getStudents.invoke().subscribeOn(Schedulers.io())
+            getStudents.invoke()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe { students -> studentList.value = students }
         )
     }
