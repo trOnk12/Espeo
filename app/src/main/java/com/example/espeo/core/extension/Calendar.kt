595f96c4919a0fadb2d360b23213d3e5f5f30deb
trOnk12 <m.pachulski94@gmail.com>
2019-11-17 13:09:12 +0100

    removed StudentStatus model

--- app/src/main/java/com/example/espeo/core/extension/Calendar.kt
+++
@@ -1,7 +0,0 @@
-package com.example.espeo.core.extension
-
-import java.util.Calendar
-
-fun Calendar.getCurrentYear():Int{
-   return Calendar.getInstance().get(Calendar.YEAR)
-}
--- app/src/main/java/com/example/espeo/domain/model/StudentStatus.kt
+++
@@ -1,6 +0,0 @@
-package com.example.espeo.domain.model
-
-enum class StudentStatus{
-    IS_STUDENT,
-    NOT_STUDENT
-}
